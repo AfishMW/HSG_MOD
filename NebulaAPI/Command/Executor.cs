9a1382ba444db69dde9a4c802a1d7b1870300a42
AfishMW <lyx20111220@126.com>
2026-07-29 18:58:39 +0800

    修改文件结构

--- NebulaAPI/Command/Executor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Virial.Command;
-
-/// <summary>
-/// 実行可能な対象を表します。
-/// </summary>
-public interface IExecutable
-{
-    CoTask<ICommandToken> CoExecute((string label, ICommandToken token)[] extra);
-}
--- NebulaPluginNova/Behavior/EmptyBehaviour.cs
+++
@@ -1,8 +0,0 @@
-﻿using Il2CppInterop.Runtime.Injection;
-
-namespace Nebula.Behavior;
-
-public class EmptyBehaviour : MonoBehaviour
-{
-    static EmptyBehaviour() => ClassInjector.RegisterTypeInIl2Cpp<EmptyBehaviour>();
-}
